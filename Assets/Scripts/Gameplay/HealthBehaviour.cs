ea0e5acd5c79d7a9c00537f19dc26b1c11b2a80a
lodis <lodisperkins44@gmail.com>
2021-01-08 10:09:19 -0600

    Added all project files

---
+++ Assets/Scripts/Gameplay/HealthBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBehaviour : MonoBehaviour
+{
+    //This game object's total health
+    private float _health;
+    //Whether or not this game object is alive
+    public bool isAlive;
+
+    // Use this for initialization
+    void Start()
+    {
+        isAlive = true;
+    }
+
+    /// <summary>
+    /// Decrements the object's health by the given value.
+    /// </summary>
+    /// <param name="damageAmount">The amount of damage to do to this game object</param>
+    public void TakeDamage(float damageAmount)
+    {
+        _health -= damageAmount;
+        if (_health <= 0)
+        {
+            isAlive = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check to see if the game object is dead.
+        if (isAlive == false)
+        {
+            //If the game object is dead, call the destroy function to remove it from the scene.
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Input/PlayerMovementBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    //The player speed
+    public float speed;
+
+    //the axis the player is moving on
+    private Vector3 moveAxis;
+
+    private void Start()
+    {
+        moveAxis = new Vector3(1, 0, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Checks to see if the button to go right has been pressed.
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            //If the button has been pressed add the moveAxis mulitplied by the speed to the position of the game object.
+            transform.position += moveAxis* speed * Time.deltaTime;
+        }
+        //Checks to see if the button to go left has been pressed.
+        else if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            //If the button was pressed add the moveAxis mulitplied by the speed to the position of the game object.
+            transform.position -= moveAxis * speed * Time.deltaTime;
+        }
+    }
+}
